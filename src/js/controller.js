fc6ac448521ac9edcd1000682a80b8588389bb7a
andrew schwirzinski <aschwirzinski@gmail.com>
2022-11-16 01:27:58 -0500

    code architecture

--- src/js/controller.js
+++ src/js/controller.js
@@ -36,6 +36,7 @@ const showRecipe = async function () {
 
     // Loading recipe
     renderSpinner(recipeContainer);
+
     const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
     const data = await res.json();
 
