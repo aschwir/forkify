95ab7ad905db372527a75c3f2cef973e1b60f17b
andrew schwirzinski <aschwirzinski@gmail.com>
2022-12-05 20:16:25 -0500

    removed fractional

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './view'
 import icons from 'url:../../img/icons.svg';
 // import { Fraction } from 'fractional';
-import { numberToFraction } from 'helpers.js'
+import { numberToFraction } from '../helpers.js'
 
 
 class RecipeView extends View {
